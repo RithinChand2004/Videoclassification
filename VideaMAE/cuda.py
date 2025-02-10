8aea8f3a104bf9b40cceb02f010ce5556be1173d
RithinChand2004 <97657716+RithinChand2004@users.noreply.github.com>
2025-02-10 22:49:34 +0530

    shifting repo

--- VideaMAE/cuda.py
+++
@@ -1,6 +0,0 @@
-import torch
-print(torch.__version__)  # Check PyTorch version
-print(torch.version.cuda)  # Check CUDA version PyTorch was built with
-print(torch.cuda.is_available())  # Should return True if CUDA is working
-print(torch.cuda.device_count())  # Should be >0 if GPU is detected
-print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU found")  # GPU name
